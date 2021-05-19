cdb51b575cf775aff4a2138e98c330089394b8fa
mm0809 <opmm0809@gmail.com>
2021-05-19 04:04:23 -0700

    update

--- HW/main.cpp
+++ HW/main.cpp
@@ -88,11 +88,11 @@ int main()
 
 
     printf("\nConnecting to %s...\r\n", MBED_CONF_APP_WIFI_SSID);
-    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
-    if (ret != 0) {
-            printf("\nConnection error: %d\r\n", ret);
-            return -1;
-    }
+    //int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+    //if (ret != 0) {
+    //        printf("\nConnection error: %d\r\n", ret);
+    //        return -1;
+    //}
 
 
     NetworkInterface* net = wifi;
@@ -106,29 +106,29 @@ int main()
     printf("Connecting to TCP network...\r\n");
 
     SocketAddress sockAddr;
-    sockAddr.set_ip_address(host);
-    sockAddr.set_port(1883);
+    //sockAddr.set_ip_address(host);
+    //sockAddr.set_port(1883);
 
-    printf("address is %s/%d\r\n", (sockAddr.get_ip_address() ? sockAddr.get_ip_address() : "None"),  (sockAddr.get_port() ? sockAddr.get_port() : 0) ); //check setting
+    //printf("address is %s/%d\r\n", (sockAddr.get_ip_address() ? sockAddr.get_ip_address() : "None"),  (sockAddr.get_port() ? sockAddr.get_port() : 0) ); //check setting
 
-    int rc = mqttNetwork.connect(sockAddr);//(host, 1883);
-    if (rc != 0) {
-            printf("Connection error.");
-            return -1;
-    }
-    printf("Successfully connected!\r\n");
+    //int rc = mqttNetwork.connect(sockAddr);//(host, 1883);
+    //if (rc != 0) {
+    //        printf("Connection error.");
+    //        return -1;
+    //}
+    //printf("Successfully connected!\r\n");
 
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-    data.MQTTVersion = 3;
-    data.clientID.cstring = "Mbed";
+    //MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    //data.MQTTVersion = 3;
+    //data.clientID.cstring = "Mbed";
 
-    if ((rc = client.connect(data)) != 0){
-            printf("Fail to connect MQTT\r\n");
-    }
+    //if ((rc = client.connect(data)) != 0){
+    //        printf("Fail to connect MQTT\r\n");
+    //}
 
-    if (client.subscribe(topicSetAngle, MQTT::QOS0, messageArrived) != 0){
-            printf("Fail to subscribe\r\n");
-    }
+    //if (client.subscribe(topicSetAngle, MQTT::QOS0, messageArrived) != 0){
+    //        printf("Fail to subscribe\r\n");
+    //}
 
     mqtt_thread.start(callback(&mqtt_queue, &EventQueue::dispatch_forever));
     //mqtt_queue.call_every(1s, &mqttHandle, &client);
